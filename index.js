951db45d95d6b67de01f279c6d9dd6e612692a0a
Tarcísio José de Amorim Carvalho <tarcisiokta2012@gmail.com>
2024-03-03 23:44:04 -0300

    Update index.js soma

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const js = "test";
-js.replace("test","Test");
+  const js = "test";
+  js.replace("test","Test");
+  const n1 = 2;
+  const n2 = 3;
+
+function soma(n1,n2){
+  return n1+n2;
+}
+
+
+const res = soma(n1,n2);
+console.log(res);
